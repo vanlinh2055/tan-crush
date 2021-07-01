f8e8d007f25dc9e5b296d0c1436b35cd8f1d7561
vanlinh2055 <86742309+vanlinh2055@users.noreply.github.com>
2021-07-01 15:24:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-introTitle: 'Babe à!',
-introDesc: `Trái đất vốn lạ thường
-Mà sao em cứ đi nhầm đường
-Lạc vào tim anh lẻ loi
-Đằng sau chữ yêu đây là thương`,
-btnIntro: 'hihi',
-title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+introTitle: 'Bé iu à!',
+introDesc: `Trái đất lạ vl
+Rộng lớn là vậy
+Mà vẫn cho đôi ta gặp nhau
+Định mệnh anh và em rồi`,
+btnIntro: 'Dạ',
+title: 'Phải chăng em đã yêu anh ngay từ cái nhìn đầu tiên 😙',
 desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-btnYes: 'Vẫn cứ là thích anh <33',
+btnYes: 'Ngoài anh ra em còn yêu ai',
 btnNo: 'Không, Anh trai à :3',
-question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+question:'Trong hàng vạn chàng trai ngoài kia, sao em lại chọn anh?',
 btnReply: 'Gửi cho anh <3',
-reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+reply: 'Yêu anh vì anh đẹp trai siêu cấp vippro',
+mess: 'Biết mà 🥰. Yêu em nhiều nha bé iu 😘😘',
+messDesc: 'Mai 1h30 anh qua đón bé nhé bé iu.',
 btnAccept: 'Okiiiii lun <3',
-messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+messLink: 'https://www.facebook.com/linhvu.marketer/' 
 }
